017d9af902c5b6bf39869d5601dda4d5ed6a4e34
Tom Chestnut <tchestnut85@gmail.com>
2020-10-27 18:10:30 -0400

    created password hashing function

--- models/User.js
+++ models/User.js
@@ -1,48 +1,49 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-// create our user model
+// create the user model
 class User extends Model { }
 
 // define table columns and configuration
 User.init(
     {
-        // ID column
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
-        // username column
         username: {
             type: DataTypes.STRING,
             allowNull: false
         },
-        // email column
         email: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
-            // allowNull set to false allows us to use a validator
             validate: {
                 isEmail: true
             }
         },
-        // password column
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                // the password must be at least 4 chars long
                 len: [4]
             }
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            }
+        },
         sequelize,
         timestamps: false,
-        freezerTableName: true,
+        freezeTableName: true,
         underscored: true,
         modelName: 'user'
     }

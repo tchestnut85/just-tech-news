17d518f63c84b9579adc6f229c94b9ece5d8d5b4
Tom Chestnut <tchestnut85@gmail.com>
2020-10-28 16:58:37 -0400

    added password hashing for updating user's password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -51,11 +51,8 @@ router.post('/', (req, res) => {
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
-    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
-        // individualHooks: true,
+        individualHooks: true,
         where: {
             id: req.params.id
         }
